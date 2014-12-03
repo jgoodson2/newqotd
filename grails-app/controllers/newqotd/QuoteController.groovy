e53fe8d8699e68488e65cf60c3ad0d4347779a00
Jonathon Goodson <goodson.jonathon@gmail.com>
2014-12-03 16:16:45 -0500

    added a 'random' view for the quote controller

--- grails-app/controllers/newqotd/QuoteController.groovy
+++ grails-app/controllers/newqotd/QuoteController.groovy
@@ -8,4 +8,9 @@ class QuoteController {
     def home(){
         render "<h1>Jonathon Goodson does not know how to program.</h1>"
     }
+    def random(){
+        def staticAuthor = "Anonymous"
+        def staticContent = "Jonathon Goodson can't program well."
+        [ author: staticAuthor, content: staticContent]
+    }
 }
---
+++ grails-app/views/quote/random.gsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jgoodson2
+  Date: 12/3/2014
+  Time: 4:15 PM
+--%>
+
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title></title>
+</head>
+
+<body>
+
+</body>
+</html>
