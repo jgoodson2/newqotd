f07a9feb11e7cd4d3ec0c4237d34d6a57d77ad9f
Jonathon Goodson <goodson.jonathon@gmail.com>
2014-12-04 09:21:26 -0500

    reworked quote controller's random method, view

--- grails-app/controllers/newqotd/QuoteController.groovy
+++ grails-app/controllers/newqotd/QuoteController.groovy
@@ -9,8 +9,11 @@ class QuoteController {
         render "<h1>Jonathon Goodson does not know how to program.</h1>"
     }
     def random(){
-        def staticAuthor = "Anonymous"
-        def staticContent = "Jonathon Goodson can't program well."
-        [ author: staticAuthor, content: staticContent]
+//        def staticAuthor = "Anonymous"
+//        def staticContent = "Jonathon Goodson can't program well."
+//        [ author: staticAuthor, content: staticContent]
+        def staticQuote = new Quote(author: 'M. Ali',
+                                    content: 'Float like a butterfly')
+        [quote:staticQuote]
     }
 }
